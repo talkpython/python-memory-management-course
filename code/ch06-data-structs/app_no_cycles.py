0a592783dc81968b394ecbff7c0f4e76fcb5cb87
Michael Kennedy <mikeckennedy@gmail.com>
2020-08-01 22:00:11 -0700

    Stage set for no cycle version.

--- code/ch06-data-structs/app_no_cycles.py
+++ code/ch06-data-structs/app_no_cycles.py
@@ -4,19 +4,25 @@ from person import Person
 
 
 def main():
-    print("VERSION WITH CYCLES!")
+    print("VERSION WITH NO CYCLES!")
     p1 = Person('Michael')
     p2 = Person('Sarah')
+    p3 = Person('Zoe')
 
     if input('Create cycle [y/n]? ') == 'y':
+        # TODO: Improve this with new data structures
         p1.friends.append(p2)
         p2.friends.append(p1)
+
+    print(f"Is {p1.name} a friend of {p2.name}? {'Yes' if p1 in p2.friends else 'No'}.")
+    print(f"Is {p1.name} a friend of {p3.name}? {'Yes' if p1 in p3.friends else 'No'}.")
 
     w1 = weakref.ref(p1)
     w2 = weakref.ref(p2)
 
     p1 = None
     p2 = None
+    p3 = None
 
     print("Program closing, kthxbye!", flush=True)
 
